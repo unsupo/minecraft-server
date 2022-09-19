d5318ecb925b33b97212b8296d9795cf979c67fd
Jonathan <unsupo@gmail.com>
2022-09-19 13:10:38 -0600

    Update minecraft-server.sh

--- minecraft-server.sh
+++ minecraft-server.sh
@@ -7,14 +7,15 @@ script=minecraft.sh
 pid=minecraft.pid
 major_version=1.19
 
+# TODO add other server types
 latest () {
 	api_output=$(curl https://api.papermc.io/v2/projects/paper/version_group/$major_version/builds) #| python -c "import sys, json; print(json.load(sys.stdin)['builds'][-1]['downloads']['application']['name'])")
 	latest_jar=$(echo $api_output | python -c "import sys, json; print(json.load(sys.stdin)['builds'][-1]['downloads']['application']['name'])")
 	latest_build=$(echo $api_output | python -c "import sys, json; print(json.load(sys.stdin)['builds'][-1]['build'])")
         latest_version=$(echo $api_output | python -c "import sys, json; print(json.load(sys.stdin)['builds'][-1]['downloads']['version'])")
-	curl https://api.papermc.io/v2/projects/paper/versions/$latest_version/builds/$latest_build/downloads/$latest_jar -o paper.jar
+	curl https://api.papermc.io/v2/projects/paper/versions/$latest_version/builds/$latest_build/downloads/$latest_jar -o server.jar
 }
-[ -e paper.jar ] && echo "paper.jar already exists skipping download...." || curl https://api.papermc.io/v2/projects/paper/versions/1.19.2/builds/138/downloads/paper-1.19.2-138.jar -o paper.jar
+[ -e server.jar ] && echo "server.jar already exists skipping download...." || latest
 
 # Set the EULA to already be TRUE
 [ -e eula.txt ] && echo "eula.txt already exists skipping download..." || curl https://raw.githubusercontent.com/unsupo/minecraft-server/main/eula.txt -o eula.txt
@@ -32,7 +33,7 @@ JVM_ARGUMENTS_RAM="-XX:G1NewSizePercent=30 -XX:G1MaxNewSizePercent=40 -XX:G1Heap
 start () {
 	# Starting the server
         echo "Starting the Server."
-        echo "java -jar -Xms${ALLOCATED_RAM} -Xmx${ALLOCATED_RAM} ${JVM_ARGUMENTS} ${JVM_ARGUMENTS_RAM} paper.jar nogui" > $script && chmod +x $script
+        echo "java -jar -Xms${ALLOCATED_RAM} -Xmx${ALLOCATED_RAM} ${JVM_ARGUMENTS} ${JVM_ARGUMENTS_RAM} server.jar nogui" > $script && chmod +x $script
         nohup ./$script & echo $! > $pid
 }
 stop () {
@@ -47,6 +48,6 @@ elif [ "restart" = "$1" ]; then
 	stop
 	start
 elif [ "update" = "$1" ]; then
-	curl https://api.papermc.io/v2/projects/paper/version_group/1.19/builds | python -c "import sys, json; print(json.load(sys.stdin)['builds'][-1]['downloads']['application']['name'])"
+	latest
 fi
 
